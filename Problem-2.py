5a4555f1d8d77635e2661ac8fbb996f67a7befce
r0jur0 <geochong0409@gmail.com>
2025-12-01 01:15:45 -0800

    Create Problem-2.py

---
+++ Problem-2.py
@@ -0,0 +1,10 @@
+
+print("Problem 2")
+a = int(input("What is the value for a? "))
+b = int(input("What is the value for b? "))
+
+
+print("Let's solve for (a - b)+(a * b):")
+answer = (a - b)+(a * b)
+print("Here is the answer: " + str(answer))
+
