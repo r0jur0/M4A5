2b1654e64063e0f765d24e19265e6785286f7dd7
r0jur0 <geochong0409@gmail.com>
2025-12-01 01:15:08 -0800

    Update and rename main.py to Problem-1.py

---
+++ Problem-1.py
@@ -0,0 +1,8 @@
+print("Problem 1")
+radius = int(input("What is the radius of the circle? "))
+radius = int(radius)
+type_of_radius = type(radius)
+print(type_of_radius)
+area = 3.14*(radius*radius)
+print(area)
+
